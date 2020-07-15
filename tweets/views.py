98b7c3cce696d3c676af902c50ec56ca6a9db95c
Luiz Fernando <luizfernandorg@gmail.com>
2020-07-14 23:11:26 -0300

    added two views and configured urls

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_view(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id):
+    print("Tweet id %d" % (tweet_id,))
+    return HttpResponse("Hello World!")
